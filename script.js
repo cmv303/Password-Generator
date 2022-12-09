d409b38d1194a3e2fe1e412281973fe3c557932f
cmv303 <115678318+cmv303@users.noreply.github.com>
2022-12-09 16:33:43 -0500

    worked on prompts and confirm boxes in js

--- script.js
+++ script.js
@@ -1,31 +1,110 @@
-// Assignment code here
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+var symbols = ["!", "#", "$", "%", "&", "*", "+", "<", "=", ">", "?", "@"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var lowerCaseLetters = upperCaseLetters.toString().toLowerCase();
+
+console.log(typeof numbers); //object
+console.log(typeof symbols); //object
+console.log(typeof lowerCaseLetters); //string
+
+// * * CANNOT SELECT NONE.
+//* * EACH prompt should generate at least 1 character, but ALL prompts should generate the whole password?? //
+
+let statusNum = confirm("Would you like to include numbers in your password?");
+if (statusNum == true) {
+  console.log("You pressed ok!")
+}
+else {
+  console.log("You pressed no!")
+
+};
+
+let statusSymb = confirm("Would you like to include symbols in your password?");
+if (statusSymb == true) {
+  console.log("You pressed ok!")
+}
+else {
+  console.log("You pressed no!")
+
+};
+
+let statusLCL = confirm("Would you like to include lowercase letters in your password?");
+if (statusLCL == true) {
+  console.log("You pressed ok!")
+}
+else {
+  console.log("You pressed no!")
+
+};
+
+let statusUCL = confirm("Would you like to include uppercase letter in your password?");
+if (statusUCL == true) {
+  console.log("You pressed ok!")
+}
+else {
+  console.log("You pressed no!")
+
+};
+
+let Password = confirm("Here's your password!");
+if (Password ==!true) {
+  alert("Start Over")
+}
+else if (Password == true) {
+  function writePassword(value) {
+    const password = generatePassword();
+    const passwordText = document.querySelector("#password");
+    passwordText.value.alert("Here you go!") = password;
+}}
 
 
+
+
+//! not working...Number of Chars needed 8-128
+ // let i = 8; i < 128;
+// if (numbers >= [8]) {
+//   console.log("Your password is long enough!")
+//   return generatePassword
+// }
+
+// ! NOT WORKING
+// let statusChar = prompt("How many characters long will your password be?");
+// if (statusChar == true) {
+//   console.log("You pressed ok!")
+// }
+
+// else {
+//   console.log("You pressed no!")
+// } 
+
+//! -- 
+// if under 8
+// return false
+// if over 128
+// return false
+// if >= 8 || <=128
+// return true
+//!
+
+
+// TODO: below //
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword(value) {
+//   const password = generatePassword();
+//   const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
+// } //TODO MESSAGE TO MYSELf, i used ^^ above as an alert....
 
-}
-
+console.log(password)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// * I need to make a series of PROMPTS:
-// TODO:  Numbers, Symbols, UpperCase, LowerCase //
-// TODO: How Many characters would you like, between 8-128 //
-// * CANNOT SELECT NONE //
-// * EACH prompt should generate at least 1 character, but ALL prompts should generate the whole password?? //
+
+
+
 // TODO: Password should be written either as an ALERT or ON THE PAGE //
 
-// ! Variables Needed
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var Numbers = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 100, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128];
-var Symbols = ["!", "#", "$", "%", "&", "*", "+", "<", "=", ">", "?", "@"];
-// var lowerCase = "lowerCase"
-// lowerCase.tolowerCase()
-// console.log(upperCase.toLowerCase) //THIS IS NOT WORKING
+
